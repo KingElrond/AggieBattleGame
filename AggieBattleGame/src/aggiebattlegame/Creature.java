9b1ef7328fac85fccad6ac85c80ae637ecc60fa7
Troy Drescher <66854137+KingElrond@users.noreply.github.com>
2021-04-29 18:13:59 -0700

    Uploaded current progress

---
+++ AggieBattleGame/src/aggiebattlegame/Creature.java
@@ -0,0 +1,13 @@
+package aggiebattlegame;
+
+public interface Creature 
+{
+  String getName();
+  String getDescription();
+  
+  // ability 1, ability 2, ability 3, super
+ 
+  int[] getStats();
+  // health,attack,defense,speed,
+  Ability[] getAbility();
+}
